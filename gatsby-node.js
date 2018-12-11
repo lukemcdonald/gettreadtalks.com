30ceb25b8d89592821dfc91aa0b837e623ad36d6
Luke McDonald <thelukemcdonald@gmail.com>
2018-12-11 14:58:45 -0600

    Fix slugs

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,20 +2,38 @@ const Promise = require(`bluebird`);
 const path = require(`path`);
 const slash = require(`slash`);
 
+const toSlug = (string) => {
+	let str = string;
+
+  str = str.replace(/^\s+|\s+$/g, ""); // trim
+  str = str.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  const from = "åàáãäâèéëêìíïîòóöôùúüûñç·/_,:;";
+  const to = "aaaaaaeeeeiiiioooouuuunc------";
+
+  for (var i = 0, l = from.length; i < l; i++) {
+    str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i));
+  }
+
+  str = str
+    .replace(/[^a-z0-9 -]/g, "") // remove invalid chars
+    .replace(/\s+/g, "-") // collapse whitespace and replace by -
+    .replace(/-+/g, "-") // collapse dashes
+    .replace(/^-+/, "") // trim - from start of text
+    .replace(/-+$/, ""); // trim - from end of text
+
+  return str;
+}
+
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
-  let slug;
 
   if (node.internal.type === `Airtable` && node.table === `Talks`) {
-    slug = `/${node.data.title.replace(/ /g, "-")
-      .replace(/[,&]/g, "")
-      .toLowerCase()}/`;
-
-    // Add slug as a field on the node.
     createNodeField({
 			node,
 			name: `slug`,
-			value: slug
+			value: toSlug(node.data.title)
 		});
   }
 };
@@ -37,7 +55,7 @@ exports.createPages = ({ graphql, actions }) => {
 			data[queryName].edges.forEach(edge => {
 				const { id, fields, data } = edge.node;
 				createPage({
-					path: `${urlPath}${fields.slug}`,
+					path: `${urlPath}/${fields.slug}`,
 					component: slash(template),
 					context: {
 						id,
