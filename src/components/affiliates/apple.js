32b525ca56717c86b2dbff56bbbee0c18928fc5e
Luke McDonald <thelukemcdonald@gmail.com>
2019-01-03 21:42:45 -0600

    Add multiple products to Apple Affiliate links

--- src/components/affiliates/apple.js
+++ src/components/affiliates/apple.js
@@ -6,12 +6,30 @@ const Container = styled.div`
 	${tw`mb-10 hidden md:block`};
 `;
 
+// @Link https://banners.itunes.apple.com/us/catalog
+const products = [
+	{
+		name: 'Saints of Zion',
+		id: '1264726262',
+		store: 'appleMusic',
+	},
+	{
+		name: 'Holiness of God',
+		id: '562309063',
+		store: 'books',
+	},
+];
+
 class Apple extends Component {
 	render() {
+		const product = products[Math.floor(Math.random() * products.length)];
+
 		return (
 			<Container>
 				<iframe
-					src="//banners.itunes.apple.com/banner.html?partnerId=&aId=&bt=catalog&t=catalog_white&id=1264726262&c=us&l=en-US&w=728&h=90&store=appleMusic"
+					src={`//banners.itunes.apple.com/banner.html?partnerId=&aId=&bt=catalog&t=catalog_white&id=${
+						product.id
+					}&c=us&l=en-US&w=728&h=90&store=${product.store}`}
 					frameBorder="0"
 					style={{
 						overflowX: 'hidden',
