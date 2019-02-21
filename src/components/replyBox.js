f071e31c4ee21e29fa854cd446ea1d31f895571a
Luke McDonald <thelukemcdonald@gmail.com>
2019-02-21 12:41:21 -0600

    Attempt to fix ReplyBox script to work on path change.

--- src/components/replyBox.js
+++ src/components/replyBox.js
@@ -1,11 +1,49 @@
-import React from 'react';
-
+import React, { Component, Fragment } from 'react';
+import Script from 'react-load-script';
 import { Box } from "./styled/box";
 
-const ReplyBox = () => (
-	<Box>
-		<div id="replybox"></div>
-	</Box>
-);
+export default class ReplyBox extends Component {
+	constructor(props) {
+		super(props);
 
-export default ReplyBox;
+		this.state = {
+			scriptError: false,
+			scriptLoaded: false,
+		}
+
+		if (typeof window !== 'undefined' && window !== null) {
+			window.replybox = {
+				site: 'rbGnNrn9v3'
+			};
+		}
+	}
+
+	handleScriptCreate() {
+		this.setState({ scriptLoaded: false })
+	}
+
+	handleScriptError() {
+		this.setState({ scriptError: true })
+	}
+
+	handleScriptLoad() {
+		this.setState({ scriptLoaded: true })
+	}
+
+	render() {
+		return (
+			<Fragment>
+				<Script
+					url="https://cdn.getreplybox.com/js/embed.js"
+					onCreate={this.handleScriptCreate.bind(this)}
+					onError={this.handleScriptError.bind(this)}
+					onLoad={this.handleScriptLoad.bind(this)}
+				/>
+
+				<Box>
+					<div id="replybox"></div>
+				</Box>
+			</Fragment>
+		);
+	}
+}
