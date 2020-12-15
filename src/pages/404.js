baf1d0f06522cf66f5cc48277583abd5d3df61d7
Luke McDonald <thelukemcdonald@gmail.com>
2020-12-15 13:52:41 -0600

    Update 404 page layout.

--- src/pages/404.js
+++ src/pages/404.js
@@ -11,30 +11,27 @@ export default function NotFoundPage() {
 
 			<Intro>
 				<Intro.Title>Are you lost?</Intro.Title>
+
 				<Intro.Tagline>
 					<p>
 						Sorry, but the page you are looking for cannot be found nor has it
 						evolved into something else.
 					</p>
 				</Intro.Tagline>
-			</Intro>
 
-			<Section>
-				<Section.Content>
-					<figure>
-						<iframe
-							title="Steve Lawson's Gospel Presentation"
-							className="rounded"
-							width="640"
-							height="360"
-							src="https://www.youtube.com/embed/4exu-7RDdKE"
-							frameBorder="0"
-							allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-							allowFullScreen
-						/>
-					</figure>
-				</Section.Content>
-			</Section>
+				<figure className="mt-10 rounded shadow-lg embed-responsive aspect-ratio-16x9">
+					<iframe
+						title="Steve Lawson's Gospel Presentation"
+						className="rounded"
+						width="640"
+						height="360"
+						src="https://www.youtube.com/embed/4exu-7RDdKE"
+						frameBorder="0"
+						allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+						allowFullScreen
+					/>
+				</figure>
+			</Intro>
 		</>
 	);
 }
