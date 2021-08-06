e5ee05fe39cbbd9c9e9f7dfacfa508a27589d100
Luke McDonald <thelukemcdonald@gmail.com>
2021-08-06 17:40:39 -0500

    Add hook to use scriptures.

---
+++ src/hooks/useScriptures.js
@@ -0,0 +1,31 @@
+import { graphql, useStaticQuery } from 'gatsby'
+
+const scripturesQuery = graphql`
+	query {
+		scriptures: allAirtableScripture {
+			nodes {
+				id
+				data {
+					verse
+					content
+				}
+			}
+		}
+	}
+`
+
+function useScriptures() {
+	const data = useStaticQuery(scripturesQuery)
+
+	const scriptures = data.scriptures.nodes.map(({ id, data }) => ({
+		id,
+		...data,
+	}))
+
+	const randomScripture =
+		scriptures[Math.floor(Math.random() * scriptures.length)]
+
+	return { scriptures, randomScripture }
+}
+
+export { useScriptures }
