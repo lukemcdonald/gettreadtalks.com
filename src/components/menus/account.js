92e9b83287885e675cac64255ce071f13fcb9497
Luke McDonald <thelukemcdonald@gmail.com>
2022-05-29 07:46:50 -0500

    fix: destructure account menu item args

--- src/components/menus/account.js
+++ src/components/menus/account.js
@@ -29,20 +29,23 @@ const navigation = [
 function AccountMenu({ className }) {
   return (
     <nav className={classNames('space-y-1 font-medium text-gray-500', className)}>
-      {navigation.map((item) => (
-        <Link
-          key={item.name}
-          to={item.to}
-          className="group flex items-center rounded-md px-2 py-2"
-          activeClassName="text-gray-900 hover:text-gray-900 opacity-100"
-        >
-          <item.icon
-            className="mr-3 -ml-1 h-6 w-6 flex-shrink-0 group-hover:text-gray-900"
-            aria-hidden="true"
-          />
-          <span className="truncate">{item.name}</span>
-        </Link>
-      ))}
+      {navigation.map((item) => {
+        const { icon: Icon, name, to } = item
+        return (
+          <Link
+            key={name}
+            to={to}
+            className="group flex items-center rounded-md px-2 py-2"
+            activeClassName="text-gray-900 hover:text-gray-900 opacity-100"
+          >
+            <Icon
+              className="mr-3 -ml-1 h-6 w-6 flex-shrink-0 group-hover:text-gray-900"
+              aria-hidden="true"
+            />
+            <span className="truncate">{name}</span>
+          </Link>
+        )
+      })}
     </nav>
   )
 }
